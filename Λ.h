844add4aed182740dee413004e82d498f792c1ff
Anna Boettcher <a.boettcher@gmail.com>
2020-06-28 18:30:27 -0700

    forward

--- Λ.h
+++ Λ.h
@@ -3,10 +3,10 @@
 #include <type_traits>
 
 constexpr decltype(auto) curry(auto&& λ) {
-  return [λ{std::move(λ)}](auto&&... α) constexpr {
-    if constexpr(std::is_invocable_v<decltype(λ), decltype(α)...>) return λ(std::move(α)...);
-    else return curry([λ{std::move(λ)}, ...α{std::move(α)}](auto&&... β) constexpr -> std::invoke_result_t<decltype(λ), decltype(α)..., decltype(β)...> {
-      return λ(std::move(α)..., std::move(β)...);
+  return [λ{std::forward(λ)}](auto&&... α) constexpr {
+    if constexpr(std::is_invocable_v<decltype(λ), decltype(α)...>) return λ(std::forward(α)...);
+    else return curry([λ{std::forward(λ)}, ...α{std::forward(α)}](auto&&... β) constexpr -> std::invoke_result_t<decltype(λ), decltype(α)..., decltype(β)...> {
+      return λ(std::forward(α)..., std::forward(β)...);
     });
   };
 };
